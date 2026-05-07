9a32c0e9fb146eecfa3d0f3478c86b8e33077303
Tom <1568512+tomcl7@users.noreply.github.com>
2026-05-07 12:25:38 -0400

    build(runtime): consolidate subsystems into rexruntime SHARED DLL

--- src/ui/rex_app.cpp
+++ src/ui/rex_app.cpp
@@ -216,7 +216,7 @@ bool ReXApp::ConstructRuntime(const PathConfig& paths) {
       input_sys->SetActiveCallback([this]() {
         if (!debug_overlay_ && !console_overlay_ && !settings_overlay_ && !shader_debugger_overlay_)
           return true;
-        return !ImGui::GetIO().WantCaptureMouse;
+        return !imgui_drawer_->GetIO().WantCaptureMouse;
       });
     }
   }
