0bc62694f9a04c25aca4a891f4018694c6a363f2
Graine25 <216443514+Graine25@users.noreply.github.com>
2026-09-14 11:15:00 -0700

    crt GetFileAttributeExA ABI swap

--- src/kernel/crt/file.cpp
+++ src/kernel/crt/file.cpp
@@ -368,8 +368,9 @@ u32 GetFileAttributesA_entry(mapped_string lpFileName) {
   return entry->attributes();
 }
 
-u32 GetFileAttributesExA_entry(u32 fInfoLevelId, mapped_string lpFileName,
+u32 GetFileAttributesExA_entry(mapped_string lpFileName, u32 fInfoLevelId,
                                mapped_void lpFileInformation) {
+  (void)fInfoLevelId;
   const char* path = static_cast<const char*>(lpFileName);
   auto* entry = REX_KERNEL_FS()->ResolvePath(path);
   if (!entry) {
